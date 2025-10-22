4e7ae9f5a6e7e8d9fe827896b9912adea42d5692
Emmanuel <emmanueltetteh.dev@gmail.com>
2025-10-22 11:53:18 -0230

    eks access entry & aws region updated

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -105,9 +105,9 @@ module "eks" {
   # Access entries (IAM Identity Center or user/role mapping)
   ##############################################
   access_entries = {
-    fusi = {
+    Emmanuel = {
       kubernetes_groups = ["eks-admins"]
-      principal_arn     = "arn:aws:iam::413026843685:user/Emmanuel"
+      principal_arn     = "arn:aws:iam::413026843685:user/NewEmmanuel"
       policy_associations = [
         {
           policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
